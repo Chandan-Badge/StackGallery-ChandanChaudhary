3f4e6428f0ea37f0fcce7b7950e19e4532cb88c5
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-04-13 07:14:43 +0530

    Uploading Basic Structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css';
+
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+
+import Slider from './components/Slider';
+import SliderLong from './components/SliderLong';
+import SliderShort from "./components/SliderShort"
+
+function App() {
+
+  return (
+    <>
+      <Slider />
+    </>
+  )
+}
+
+export default App
