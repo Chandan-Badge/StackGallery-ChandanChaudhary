b35401fcc39b781d5832df6f9138de5f2092b586
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-05-27 19:56:36 +0530

    Improving the Profile

--- src/components/Profilepage/Profile.jsx
+++ src/components/Profilepage/Profile.jsx
@@ -10,10 +10,12 @@ function Profile() {
                 <HomeHeader />
                 <HomeDesign />
 
-                <div className="w-[90vw] sm:w-[70vw] h-[70vh] mt-10 px-6 sm:px-10 py-8 flex justify-center items-center bg-gradient-to-b from-black/50 to-transparent backdrop-blur-sm rounded-md shadow-[0_0_15px_rgba(0,0,0,0.2)] border border-yellow-300/20">
-                    <div class="w-full">
-                        <h2 className="text-yellow-300 font-bold text-lg sm:text-2xl">OVERVIEW</h2>
-                        <p className="mt-1 md:mt-2 text-[#fff] text-sm lg:text-base">Aspiring and results-oriented Student with a strong desire to learn and grow in the tech industry. passion for innovative technology solutions and a solid foundation in programming, algorithms, and software development.</p>
+                <div className="w-[90vw] sm:w-[70vw] mt-10 px-6 sm:px-10 py-8 flex justify-center items-center bg-gradient-to-b from-black/50 to-transparent backdrop-blur-sm rounded-md shadow-[0_0_15px_rgba(0,0,0,0.2)] border border-yellow-300/20">
+                    <div className="w-full">
+                        <h2 className="text-yellow-300 font-bold text-lg sm:text-2xl">About me</h2>
+                        <p className="max-w-[50vw] mt-1 md:mt-2 text-[#fff] text-sm lg:text-base">I'm Chandan Chaudhary, a 18-year-old Indian <span className="text-yellow-300">Freelance & MERN Stack developer</span>. I'm a weird guy who likes innovative things with web technologies.
+                            <br />I like to <span className="text-yellow-300">resolve</span> frontend & backend problems, <span className="text-yellow-300">create</span> smart user interface and <span className="text-yellow-300">imagine</span> useful interaction, developing rich <span className="text-yellow-300">web experiences</span> & <span className="text-yellow-300">web applications</span>.
+                            <br />When my code is not working or futzing around the code, I study how to <span className="text-yellow-300">resolve</span> it.</p>
 
                         {/* <div className="flex flex-row gap-4 lg:0 mt-2">
                             <div className="text-yellow-300 text-xs md:text-sm lg:text-base font-bold flex flex-col ">
@@ -57,10 +59,10 @@ function Profile() {
                                 <td className="font-normal text-gray-300 text-xs md:text-sm lg:text-base pl-2 sm:pl-6">Bachelor of Computer Applications</td>
                             </tr>
 
-                            <tr>
+                            {/* <tr>
                                 <td className="text-yellow-300 text-xs md:text-sm lg:text-base font-bold flex">Age:</td>
                                 <td className="font-normal text-gray-300 text-xs md:text-sm lg:text-base pl-2 sm:pl-6">18</td>
-                            </tr>
+                            </tr> */}
 
                             <tr>
                                 <td className="text-yellow-300 text-xs md:text-sm lg:text-base font-bold flex">Freelance:</td>
@@ -72,7 +74,48 @@ function Profile() {
                             </tr> */}
                         </table>
 
-                        <h2 className="text-yellow-300 font-bold text-xl sm:text-2xl mt-4 md:mt-4">MERN STACK</h2>
+
+                        <ul className="flex flex-wrap gap-2 md:gap-3 mt-2 md:mt-8 text-[#333] dark:text-[#fff]">
+
+                            <li className="overflow-x-visible relative w-10 md:w-14 h-12 md:h-[60px] overflow-y-clip group text-center flex items-center justify-center">
+                                <div className="flex justify-center items-center w-8 md:w-10 h-8 md:h-10 rounded-full transition-all duration-150 absolute top-0 group-hover:origin-top text-white">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-[#111] dark:fill-[#fff] group-hover:fill-[#7cdfff] dark:group-hover:fill-[#7cdfff]" viewBox="0 0 512 512"><path d="M418.2 177.2c-5.4-1.8-10.8-3.5-16.2-5.1 .9-3.7 1.7-7.4 2.5-11.1 12.3-59.6 4.2-107.5-23.1-123.3-26.3-15.1-69.2 .6-112.6 38.4-4.3 3.7-8.5 7.6-12.5 11.5-2.7-2.6-5.5-5.2-8.3-7.7-45.5-40.4-91.1-57.4-118.4-41.5-26.2 15.2-34 60.3-23 116.7 1.1 5.6 2.3 11.1 3.7 16.7-6.4 1.8-12.7 3.8-18.6 5.9C38.3 196.2 0 225.4 0 255.6c0 31.2 40.8 62.5 96.3 81.5 4.5 1.5 9 3 13.6 4.3-1.5 6-2.8 11.9-4 18-10.5 55.5-2.3 99.5 23.9 114.6 27 15.6 72.4-.4 116.6-39.1 3.5-3.1 7-6.3 10.5-9.7 4.4 4.3 9 8.4 13.6 12.4 42.8 36.8 85.1 51.7 111.2 36.6 27-15.6 35.8-62.9 24.4-120.5-.9-4.4-1.9-8.9-3-13.5 3.2-.9 6.3-1.9 9.4-2.9 57.7-19.1 99.5-50 99.5-81.7 0-30.3-39.4-59.7-93.8-78.4zM282.9 92.3c37.2-32.4 71.9-45.1 87.7-36 16.9 9.7 23.4 48.9 12.8 100.4-.7 3.4-1.4 6.7-2.3 10-22.2-5-44.7-8.6-67.3-10.6-13-18.6-27.2-36.4-42.6-53.1 3.9-3.7 7.7-7.2 11.7-10.7zM167.2 307.5c5.1 8.7 10.3 17.4 15.8 25.9-15.6-1.7-31.1-4.2-46.4-7.5 4.4-14.4 9.9-29.3 16.3-44.5 4.6 8.8 9.3 17.5 14.3 26.1zm-30.3-120.3c14.4-3.2 29.7-5.8 45.6-7.8-5.3 8.3-10.5 16.8-15.4 25.4-4.9 8.5-9.7 17.2-14.2 26-6.3-14.9-11.6-29.5-16-43.6zm27.4 68.9c6.6-13.8 13.8-27.3 21.4-40.6s15.8-26.2 24.4-38.9c15-1.1 30.3-1.7 45.9-1.7s31 .6 45.9 1.7c8.5 12.6 16.6 25.5 24.3 38.7s14.9 26.7 21.7 40.4c-6.7 13.8-13.9 27.4-21.6 40.8-7.6 13.3-15.7 26.2-24.2 39-14.9 1.1-30.4 1.6-46.1 1.6s-30.9-.5-45.6-1.4c-8.7-12.7-16.9-25.7-24.6-39s-14.8-26.8-21.5-40.6zm180.6 51.2c5.1-8.8 9.9-17.7 14.6-26.7 6.4 14.5 12 29.2 16.9 44.3-15.5 3.5-31.2 6.2-47 8 5.4-8.4 10.5-17 15.5-25.6zm14.4-76.5c-4.7-8.8-9.5-17.6-14.5-26.2-4.9-8.5-10-16.9-15.3-25.2 16.1 2 31.5 4.7 45.9 8-4.6 14.8-10 29.2-16.1 43.4zM256.2 118.3c10.5 11.4 20.4 23.4 29.6 35.8-19.8-.9-39.7-.9-59.5 0 9.8-12.9 19.9-24.9 29.9-35.8zM140.2 57c16.8-9.8 54.1 4.2 93.4 39 2.5 2.2 5 4.6 7.6 7-15.5 16.7-29.8 34.5-42.9 53.1-22.6 2-45 5.5-67.2 10.4-1.3-5.1-2.4-10.3-3.5-15.5-9.4-48.4-3.2-84.9 12.6-94zm-24.5 263.6c-4.2-1.2-8.3-2.5-12.4-3.9-21.3-6.7-45.5-17.3-63-31.2-10.1-7-16.9-17.8-18.8-29.9 0-18.3 31.6-41.7 77.2-57.6 5.7-2 11.5-3.8 17.3-5.5 6.8 21.7 15 43 24.5 63.6-9.6 20.9-17.9 42.5-24.8 64.5zm116.6 98c-16.5 15.1-35.6 27.1-56.4 35.3-11.1 5.3-23.9 5.8-35.3 1.3-15.9-9.2-22.5-44.5-13.5-92 1.1-5.6 2.3-11.2 3.7-16.7 22.4 4.8 45 8.1 67.9 9.8 13.2 18.7 27.7 36.6 43.2 53.4-3.2 3.1-6.4 6.1-9.6 8.9zm24.5-24.3c-10.2-11-20.4-23.2-30.3-36.3 9.6 .4 19.5 .6 29.5 .6 10.3 0 20.4-.2 30.4-.7-9.2 12.7-19.1 24.8-29.6 36.4zm130.7 30c-.9 12.2-6.9 23.6-16.5 31.3-15.9 9.2-49.8-2.8-86.4-34.2-4.2-3.6-8.4-7.5-12.7-11.5 15.3-16.9 29.4-34.8 42.2-53.6 22.9-1.9 45.7-5.4 68.2-10.5 1 4.1 1.9 8.2 2.7 12.2 4.9 21.6 5.7 44.1 2.5 66.3zm18.2-107.5c-2.8 .9-5.6 1.8-8.5 2.6-7-21.8-15.6-43.1-25.5-63.8 9.6-20.4 17.7-41.4 24.5-62.9 5.2 1.5 10.2 3.1 15 4.7 46.6 16 79.3 39.8 79.3 58 0 19.6-34.9 44.9-84.8 61.4zm-149.7-15c25.3 0 45.8-20.5 45.8-45.8s-20.5-45.8-45.8-45.8c-25.3 0-45.8 20.5-45.8 45.8s20.5 45.8 45.8 45.8z"/></svg>
+                                </div>
+                                <div className="absolute -bottom-10 left-1/2 text-[10px] md:text-xs text-center text-black dark:text-[#fff] whitespace-nowrap transition-all duration-300 ease-in-out transform -translate-x-1/2 group-hover:bottom-0">
+                                    React.js
+                                </div>
+                            </li>
+
+                            <li className="overflow-x-visible relative w-10 md:w-14 h-12 md:h-[60px] overflow-y-clip group text-center flex items-center justify-center">
+                                <div className="flex justify-center items-center w-8 md:w-10 h-8 md:h-10 rounded-full transition-all duration-150 absolute top-0 group-hover:origin-top text-white">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-[#111] dark:fill-[#fff] group-hover:fill-[#7b218c] dark:group-hover:fill-[#9d4edd]" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
+                                </div>
+                                <div className="absolute -bottom-10 left-1/2 text-[10px] md:text-xs text-center text-black dark:text-[#fff] whitespace-nowrap transition-all duration-300 ease-in-out transform -translate-x-1/2 group-hover:bottom-0">
+                                    Git & GitHub
+                                </div>
+                            </li>
+
+                            <li className="overflow-x-visible relative w-10 md:w-14 h-12 md:h-[60px] overflow-y-clip group text-center flex items-center justify-center">
+                                <div className="flex justify-center items-center w-8 md:w-10 h-8 md:h-10 rounded-full transition-all duration-150 absolute top-0 group-hover:origin-top text-white">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-[#111] dark:fill-[#fff] group-hover:fill-[#0f6dbf]" viewBox="0 0 576 512"><path d="M9.4 86.6C-3.1 74.1-3.1 53.9 9.4 41.4s32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L178.7 256 9.4 86.6zM256 416l288 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-288 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/></svg>
+                                </div>
+                                <div className="absolute -bottom-10 left-1/2 text-[10px] md:text-xs text-center text-black dark:text-[#fff] whitespace-nowrap transition-all duration-300 ease-in-out transform -translate-x-1/2 group-hover:bottom-0">
+                                    Terminal
+                                </div>
+                            </li>
+
+                            <li className="overflow-x-visible relative w-10 md:w-14 h-12 md:h-[60px] overflow-y-clip group text-center flex items-center justify-center">
+                                <div className="flex justify-center items-center w-8 md:w-10 h-8 md:h-10 p-1.5 rounded-full transition-all duration-150 absolute top-0 group-hover:origin-top text-white">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-max fill-[#111] dark:fill-[#fff] group-hover:fill-[#ff3737]" viewBox="0 0 384 512"><path d="M14 95.8C14 42.9 56.9 0 109.8 0H274.2C327.1 0 370 42.9 370 95.8C370 129.3 352.8 158.8 326.7 175.9C352.8 193 370 222.5 370 256C370 308.9 327.1 351.8 274.2 351.8H272.1C247.3 351.8 224.7 342.4 207.7 326.9V415.2C207.7 468.8 163.7 512 110.3 512C57.5 512 14 469.2 14 416.2C14 382.7 31.2 353.2 57.2 336.1C31.2 319 14 289.5 14 256C14 222.5 31.2 193 57.2 175.9C31.2 158.8 14 129.3 14 95.8zM176.3 191.6H109.8C74.2 191.6 45.4 220.4 45.4 256C45.4 291.4 74 320.2 109.4 320.4C109.5 320.4 109.7 320.4 109.8 320.4H176.3V191.6zM207.7 256C207.7 291.6 236.5 320.4 272.1 320.4H274.2C309.7 320.4 338.6 291.6 338.6 256C338.6 220.4 309.7 191.6 274.2 191.6H272.1C236.5 191.6 207.7 220.4 207.7 256zM109.8 351.8C109.7 351.8 109.5 351.8 109.4 351.8C74 352 45.4 380.8 45.4 416.2C45.4 451.7 74.6 480.6 110.3 480.6C146.6 480.6 176.3 451.2 176.3 415.2V351.8H109.8zM109.8 31.4C74.2 31.4 45.4 60.2 45.4 95.8C45.4 131.4 74.2 160.2 109.8 160.2H176.3V31.4H109.8zM207.7 160.2H274.2C309.7 160.2 338.6 131.4 338.6 95.8C338.6 60.2 309.7 31.4 274.2 31.4H207.7V160.2z"/></svg>
+                                </div>
+                                <div className="absolute -bottom-10 left-1/2 text-[10px] md:text-xs text-center text-black dark:text-[#fff] whitespace-nowrap transition-all duration-300 ease-in-out transform -translate-x-1/2 group-hover:bottom-0">
+                                    Figma
+                                </div>
+                            </li>
+
+                        </ul>
+
+                        {/* <h2 className="text-yellow-300 font-bold text-xl sm:text-2xl mt-4 md:mt-4">MERN STACK</h2>
                         <div className="relative flex mt-12 gap-8">
 
                             <span className="relative flex flex-col items-center group">
@@ -143,7 +186,7 @@ function Profile() {
                                 </svg>
                                 <p className="mt-4 text-lg sm:text-xl text-[#8CC84B]">N</p>
                             </span>
-                        </div> 
+                        </div>  */}
                     </div>
                 </div>
             </div>
