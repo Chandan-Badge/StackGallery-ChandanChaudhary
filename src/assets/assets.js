4d0a090f6800e338462aaa0f35f7552fd668071b
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-08-06 19:38:40 +0530

    importing images through assets.js

---
+++ src/assets/assets.js
@@ -0,0 +1,64 @@
+import c2explains from "./ProjectImages/c2explains.png";
+import safesphere from "./ProjectImages/SafeSphere.png";
+import portfolio from "./ProjectImages/Portfolio.png";
+import strongPassword from "./ProjectImages/StrongPassword.png";
+import strongPasswordDark from "./ProjectImages/StrongPasswordDark.png";
+import linktree from "./ProjectImages/LinkTree.png";
+
+import aichatBot from "./ProjectImages/AiChatBot.png";
+import bgChanger from "./ProjectImages/bgChanger.png";
+
+import autoTyper from "./ProjectImages/AutoTyper.png";
+import catFact from "./ProjectImages/CatFact.png";
+import darkLight from "./ProjectImages/Dark&Light.png";
+import linkTree from "./ProjectImages/LinkTree.png"
+import multiTab from "./ProjectImages/MultiTab.png";
+import randomColor from "./ProjectImages/RandomColor.png";
+import randomDog from "./ProjectImages/RandomDog.png";
+import aiChatBot from "./ProjectImages/AiChatBot.png";
+import simonSays from "./ProjectImages/SimonSays.png";
+import slider from "./ProjectImages/Slider.png";
+import tictactoe from "./ProjectImages/TicTacToe.png";
+import transform from "./ProjectImages/Transform.png";
+
+
+import university from "./ProjectImages/University.png";
+import amazon from "./ProjectImages/amazon.png";
+import netflix from "./ProjectImages/Netflix.png";
+import tooltip from "./ProjectImages/ToolTip.png";
+
+import chatApp from "./ProjectImages/ChattingApplication.png";
+
+export const assets = {
+    
+    c2explains,
+    safesphere,
+    portfolio,
+    strongPassword,
+    strongPasswordDark,
+    linktree,
+
+    aichatBot,
+    bgChanger,
+
+    autoTyper,
+    catFact,
+    darkLight,
+    linkTree,
+    multiTab,
+    randomColor,
+    randomDog,
+    aiChatBot,
+    simonSays,
+    slider,
+    tictactoe,
+    transform,
+    
+    university,
+    amazon,
+    netflix,
+    tooltip,
+
+    chatApp,
+
+}
