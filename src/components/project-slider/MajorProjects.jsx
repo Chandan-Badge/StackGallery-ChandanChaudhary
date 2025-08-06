41150834d73cc64cf292961619fc2478948e54a6
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-08-06 19:29:37 +0530

    adding c2 project

--- src/components/project-slider/MajorProjects.jsx
+++ src/components/project-slider/MajorProjects.jsx
@@ -8,6 +8,7 @@ import portfolio from "../../assets/ProjectImages/Portfolio.png";
 import strongPassword from "../../assets/ProjectImages/StrongPassword.png";
 import strongPasswordDark from "../../assets/ProjectImages/StrongPasswordDark.png";
 import linktree from "../../assets/ProjectImages/LinkTree.png";
+import c2explains from "../../assets/ProjectImages/c2explains.png";
 
 import EmptyProject from './EmptyProject';
 
@@ -76,6 +77,16 @@ export default function MajorProjects() {
                       <p className='text-[10px] md:text-xs'>Women's safety application</p>
                     </span>
                   </a></SwiperSlide>
+
+                  <SwiperSlide className='swiper-slide text-center text-base flex justify-center items-center rounded-md md:rounded-lg p-2 md:p-4'>
+                    <a href="https://chandanchaudhary-soon.netlify.app/" className='flex flex-col h-max gap-1 sm:gap-2' target="_blank">
+                      <img src={c2explains} className='h-[90%] hover:scale-105 transition-all duration-200 ease-in-out rounded-md md:rounded-lg border-[3px] md:border-[6px] border-transparent hover:border-[#b6bee5]/40' alt="" />
+                      <span className='h-max'>
+                        <h3 className='font-bold text-sm md:text-lg text-orange-600'>C2 Explains</h3>
+                        <p className='text-[10px] md:text-xs '>Personal Website</p>
+                      </span>
+                    </a>
+                  </SwiperSlide>
 
                   <SwiperSlide className='swiper-slide text-center text-base flex justify-center items-center rounded-md md:rounded-lg p-2 md:p-4'>
                   <a href="https://strong-password-generator-c2.netlify.app/" className='flex flex-col h-max gap-1 sm:gap-2' target="_blank">
